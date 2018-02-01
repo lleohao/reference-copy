9e696c6318e9970fcafb40bfa5019d186d3afbad
lleohao <lleohao@hotmail.com>
2018-02-01 14:19:40 +0800

    feat: 添加引用复制功能

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,7 +1,43 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
+const {contextMenus} = chrome;
 
-console.log('\'Allo \'Allo! Event Page');
+function copyToClipboard (text) {
+  const copyDiv = document.createElement('div');
+  copyDiv.contentEditable = true;
+  document.body.appendChild(copyDiv);
+  copyDiv.innerHTML = text;
+  copyDiv.unselectable = 'off';
+  copyDiv.focus();
+  document.execCommand('SelectAll');
+  document.execCommand('Copy', false, null);
+  document.body.removeChild(copyDiv);
+}
+
+function createLinkText (text, url) {
+  return `<a href="${url}" target="_blank">${decodeURIComponent(text)}</a>`;
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  contextMenus.create({
+    title: '引用复制',
+    contexts: ['selection', 'page'],
+    onclick: (info, tab) => {
+      const {selectionText} = info;
+      const {title, url} = tab;
+      
+      let text;
+      
+      /**
+       * 未选中 selectionText 为 undefined
+       */
+      if (selectionText === undefined) {
+        text = title;
+      } else {
+        text = selectionText;
+      }
+      
+      copyToClipboard(createLinkText(text, url));
+    }
+  });
+});
